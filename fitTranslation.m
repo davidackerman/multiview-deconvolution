0d321b46ab101d2e504a13c2325fd89ead335014
Fernando Amat <famat81@gmail.com>
2015-05-28 08:51:34 -0400

    Fixed minor bug in fit translation code

--- fitTranslation.m
+++ fitTranslation.m
@@ -6,17 +6,25 @@ function [A, nccVal] = fitTranslation(imRef, im, winRadius)
 
 [x,y,z] = ind2sub(size(im),pos);
 
+x = max(winRadius+1,x);
+y = max(winRadius+1,y);
+z = max(winRadius+1,z);
+x = min(size(im,1)-winRadius-1,x);
+y = min(size(im,2)-winRadius-1,y);
+z = min(size(im,3)-winRadius-1,z);
+
 imBlock = im(x-winRadius+1:x+winRadius,y-winRadius+1:y+winRadius,z-winRadius+1:z+winRadius);
 
 %find translation
-Taux = imRegistrationTranslationMultipleHypothesis(imRef, imBlock, 1, 0);
+Taux = imRegistrationTranslationMultipleHypothesis(imRef, imBlock, 1, 0.7);%it has to be a good match
 
 if( isempty(Taux) )
     Taux = zeros(1,4);
+else
+    offset = [x,y,z] - size(imRef)/2;
+    Taux(1:3) = Taux(1:3) + offset;
 end
 
-offset = [x,y,z] - size(imRef)/2;
-Taux(1:3) = Taux(1:3) + offset;
 
 A = eye(4);
 A(4,1:3) = -Taux([2 1 3]);
