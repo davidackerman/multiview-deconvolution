537b6c92e752e6c942482573f79ef0fbf0d7f581
Adam L. Taylor <taylora@janelia.hhmi.org>
2016-12-14 11:54:42 -0500

    adam: Changed commit of KLB submodule to one that's merged with ngc-cmake.  Fixed some files to cope with necessary changes to KLB.

--- test-imwarp-flexible-cpp/modpath.m
+++ test-imwarp-flexible-cpp/modpath.m
@@ -2,6 +2,6 @@ function modpath()
     absolute_path_to_this_file = mfilename('fullpath') ;
     absolute_path_to_this_dir = fileparts(absolute_path_to_this_file) ;
     absolute_path_to_mvd_repo = fileparts(absolute_path_to_this_dir) ;
-    absolute_path_to_klb_matlab_wrappers = fullfile(absolute_path_to_mvd_repo, 'CUDA/build/src/KLB/Debug') ;
+    absolute_path_to_klb_matlab_wrappers = fullfile(absolute_path_to_mvd_repo, 'CUDA/build/src/KLB/mex/Debug') ;
     addpath(absolute_path_to_klb_matlab_wrappers) ;
 end
