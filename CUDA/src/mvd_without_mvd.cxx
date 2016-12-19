40b8ef729b19bc5289f682b61c4fbf3c5b59e4ea
Adam L. Taylor <taylora@hhmi.org>
2016-12-19 16:25:36 -0500

    adam: executables that use MVD library no longer get included in the library.

--- CUDA/src/mvd_without_mvd.cxx
+++ CUDA/src/mvd_without_mvd.cxx
@@ -23,6 +23,8 @@ typedef std::chrono::high_resolution_clock Clock;
 
 int main(int argc, const char** argv)
 {
+    int err = 0 ;
+
 	auto tStart = Clock::now();
 	auto t1 = Clock::now();
 	auto t2 = Clock::now();
@@ -53,7 +55,8 @@ int main(int argc, const char** argv)
 		//master.full_img_mem.readImage(master.paramDec.fileImg[ii], -1);		
 
 		cout << "Reading PSF for view " << ii << endl;
-		master.full_psf_mem.readImage(master.paramDec.filePSF[ii], -1);
+		err = master.full_psf_mem.readImage(master.paramDec.filePSF[ii], -1);
+        if (err) return err ;
 
 		t2 = Clock::now();
 		std::cout << "Took " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms" << std::endl;
@@ -75,7 +78,7 @@ int main(int argc, const char** argv)
 	//dimsOut[2] = ceil(master.full_img_mem.dimsImgVec[refView].dims[2] * master.paramDec.anisotropyZ);
 
 	//find out best dimension to perform blocks for minimal padding
-	int err = master.findBestBlockPartitionDimension_inMem();
+	err = master.findBestBlockPartitionDimension_inMem();
 	if (err > 0)
 		return err;
 
