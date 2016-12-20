a0fa8017a3c63eb34d5738b384f4936791937419
Adam L. Taylor <taylora@hhmi.org>
2016-12-20 16:03:01 -0500

    adam: Changes to get everything building again.

--- CUDA/src/external/imwarp_flexible/imwarp_flexible.c
+++ CUDA/src/external/imwarp_flexible/imwarp_flexible.c
@@ -12,7 +12,30 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "image_interpolation_ml.h"
-#include "multiple_os_thread.h"
+//#include "multiple_os_thread.h"
+
+//
+// This stuff if for cross-platform threads
+//
+/*   undef needed for LCC compiler  */
+#undef EXTERN_C
+#ifdef _WIN32
+#include <windows.h>
+#include <process.h>
+#define voidthread unsigned __stdcall
+#define EndThread _endthreadex(0); return 0
+#define ThreadHANDLE HANDLE
+#define WaitForThreadFinish(t) WaitForSingleObject(t, INFINITE);CloseHandle(t)
+#define StartThread(a,b,c) a=(HANDLE)_beginthreadex(NULL, 0, b, c, 0, NULL );
+#else
+#include <pthread.h>
+#define voidthread void
+#define EndThread pthread_exit(NULL)
+#define ThreadHANDLE pthread_t
+#define WaitForThreadFinish(t) pthread_join(t, NULL)
+#define StartThread(a,b,c) pthread_create((pthread_t*)&a, NULL, (void*)b, c);
+#endif
+
 
 #define AFFINE_3D_MATRIX_SIZE 16
 
